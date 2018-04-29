13505cb603860cad69a60987a41db10f5cd2ef33
Ross Salge <rossnsalge@gmail.com>
2018-04-29 17:08:51 -0500

    Added comments in CurrentPlayer for when I come back to project

--- client/src/components/CurrentPlayer.js
+++ client/src/components/CurrentPlayer.js
@@ -1,5 +1,10 @@
 import React from "react";
 import _ from "lodash";
+
+/*this component will need to be a container and have refernce to players in redux state,
+will need to add action creator for UPDATE_PLAYER that will be called once values are cahanged
+and the next player is called to CurrentPlayer
+*/
 const CurrentPlayer = props => {
   let headers = Object.keys(props.player);
   return (
