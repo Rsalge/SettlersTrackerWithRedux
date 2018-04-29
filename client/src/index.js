0bb15472389779b5a1f022c6ff14dc21cb3b3565
Ross Salge <rossnsalge@gmail.com>
2018-04-29 13:58:40 -0500

    Adds react-redux, redux, react-router-dom, redux-promise as dependencies. Sets ups index.js for redux and react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/games/:id" component={PreviousGames} />
+          {/*Needs to be after /posts/new since it has the wildcard ":id" condition */}
+          <Route path="/" component={CurrentGame} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
