5157b8bc6ee488aa89371c94a5c0a6000cbc0da6
Ross Salge <rossnsalge@gmail.com>
2018-05-01 18:15:09 -0500

    Creation of createGame action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const ADD_PLAYER = "add_player";
 export const FETCH_PREVIOUS_GAMES = "fetch_previous_games";
+export const CREATE_GAME = "create_game";
 
 export function addPlayer(player) {
   return {
@@ -13,6 +14,17 @@ export function fetchGames() {
   const request = axios.get("/api/games");
   return {
     type: FETCH_PREVIOUS_GAMES,
+    payload: request
+  };
+}
+
+export function createGame(gameInfo, callback) {
+  const request = axios
+    .post("/api/createGame", gameInfo, callback)
+    .then(() => callback());
+
+  return {
+    type: CREATE_GAME,
     payload: request
   };
 }
