40a0fa5b66af6c9601a0a9ca1f135620c9b19761
Ross Salge <rossnsalge@gmail.com>
2019-09-17 22:05:37 -0500

    Update Home welcome message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,12 +5,16 @@ const Home = () => {
     <div className="home">
       <h3>Welcome to Settlers Tracker!</h3>
       <p>
-        The purpose of this app is to provide a player the ability to track the
-        stats of a game of Settlers of Catan as you are playing, on a turn by
-        turn basis. Previously completed games can be viewed under the Previous
-        Games tab, allowing you to see the previous game stats. Just a fun way
-        to keep up with how games went, who won, how many f***ing fours were
-        rolled, etc.
+        The main purpose of this app is to track what dice have been rolled
+        during a game of catan, at least that is what most interested me. In
+        addition to tracking each dice roll per turn you can also keep track of
+        a players Victory Points (VP) by tracking the number of settlements,
+        cities, roads, and ports a player has. Largest Army, Longest Road, and
+        Harbor Master are additional objectives that award 2 VPs each and the
+        holder of each objective is tracked as well. Previously completed games
+        can be viewed under the Previous Games tab, allowing you to see the
+        previous game stats. Just a fun way to keep up with how games went, who
+        won, how many f***ing fours were rolled, etc.
       </p>
     </div>
   );
