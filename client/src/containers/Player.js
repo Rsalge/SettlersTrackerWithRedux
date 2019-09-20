8339cea559168d56de77d9bf7268f69cebe53968
Ross Salge <rossnsalge@gmail.com>
2019-09-19 20:57:02 -0500

    Use FieldEnum values instead of hardcoded strings

--- client/src/containers/Player.js
+++ client/src/containers/Player.js
@@ -1,8 +1,14 @@
 import React from "react";
+import FieldEnum from "../components/enums/FieldEnum";
 import FieldList from "../components/FieldList";
 
 const Player = props => {
-  const fields = ["settlements", "cities", "roadLength"];
+  const fields = [
+    FieldEnum.SETTLEMENT_COUNT,
+    FieldEnum.CITY_COUNT,
+    FieldEnum.ROAD_COUNT,
+    FieldEnum.KNIGHT_COUNT
+  ];
   return (
     <div className="player" style={{ backgroundColor: props.player.color }}>
       <FieldList game={props.game} player={props.player} fields={fields} />
