ef392b2e1b585644b1d953f0ec23cd27d0f58f29
Ross Salge <rossnsalge@gmail.com>
2019-09-19 20:54:25 -0500

    Implement FieldEnum for player field names

---
+++ client/src/components/enums/FieldEnum.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FieldEnum = {
+  KNIGHT_COUNT: "knights",
+  SETTLEMENT_COUNT: "settlements",
+  CITY_COUNT: "cities",
+  ROAD_COUNT: "roadLength",
+  DICE_ROLL: "diceRoll",
+  NEW_LAND: "plusOne",
+  HARBOR_COUNT: "harbors"
+};
+
+export default FieldEnum;
