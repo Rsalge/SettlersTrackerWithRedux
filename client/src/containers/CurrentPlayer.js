53cfab037c794b1e67273aaf2437fa71409e8004
Ross Salge <rsalge@securelink.com>
2019-09-20 21:19:11 -0500

    Trying to get timer to work

--- client/src/containers/CurrentPlayer.js
+++ client/src/containers/CurrentPlayer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import FieldList from "../components/FieldList";
+import Timer from "react-compound-timer";
 class CurrentPlayer extends Component {
   render() {
     if (!this.props.players || this.props.players.length === 0)
@@ -11,8 +12,7 @@ class CurrentPlayer extends Component {
     return (
       <div className="currentPlayer" style={{ backgroundColor: player.color }}>
         <div className="fieldList">
-          <h1 className="leadingField">{player.name}</h1>
-          <h1>Testing</h1>
+        <h1 className="leadingField">{player.name}</h1>
         </div>
         <FieldList
           game={this.props.game}
