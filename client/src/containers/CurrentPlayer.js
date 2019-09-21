319887171ebc849a973d298d53687565c73ce38d
Ross Salge <rossnsalge@gmail.com>
2019-09-20 21:40:00 -0500

    Display timer, not saving to turn yet

--- client/src/containers/CurrentPlayer.js
+++ client/src/containers/CurrentPlayer.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import FieldList from "../components/FieldList";
-import Timer from "react-compound-timer";
+import Timer from "react-timer-wrapper";
+import Timecode from "react-timecode";
+
 class CurrentPlayer extends Component {
   render() {
     if (!this.props.players || this.props.players.length === 0)
@@ -12,8 +14,14 @@ class CurrentPlayer extends Component {
     return (
       <div className="currentPlayer" style={{ backgroundColor: player.color }}>
         <div className="fieldList">
-        <h1 className="leadingField">{player.name}</h1>
+          <h1 className="leadingField">{player.name}</h1>
+          <div className="leadingField">
+            <Timer active duration={null}>
+              <Timecode />
+            </Timer>
+          </div>
         </div>
+
         <FieldList
           game={this.props.game}
           player={player}
